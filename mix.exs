bc6e97e5a3870989cf2e9e12ce3378026a31d82f
Tony Pitale <tpitale@gmail.com>
2018-09-11 13:01:59 -0500

    Start dynamic supervisor

--- mix.exs
+++ mix.exs
@@ -14,6 +14,7 @@ defmodule Coap.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
+      mod: {CoAP, []},
       extra_applications: [:logger]
     ]
   end
