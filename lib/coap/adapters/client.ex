08f0c7b63689c2ddc5194f80fb619c085b37b93f
Tony Pitale <tpitale@gmail.com>
2019-03-19 14:48:06 -0700

    Handle ack from server for client.

--- lib/coap/adapters/client.ex
+++ lib/coap/adapters/client.ex
@@ -2,4 +2,8 @@ defmodule CoAP.Adapters.Client do
   def response(message, {client, peer}, _connection) do
     send(client, {:deliver, message, peer})
   end
+
+  def ack({client, _}) do
+    send(client, :ack)
+  end
 end
--- lib/coap/client.ex
+++ lib/coap/client.ex
@@ -24,7 +24,6 @@ defmodule CoAP.Client do
   def request(type, method, url, content) do
     uri = :uri_string.parse(url)
 
-    # TODO: what if this is a hostname?
     host = uri[:host]
     port = uri[:port]
     token = :crypto.strong_rand_bytes(4)
@@ -42,22 +41,18 @@ defmodule CoAP.Client do
       options: %{uri_path: String.split(uri[:path], "/")}
     }
 
-    # TODO: peer = {ip, port, token} = Message.peer_from(message)
-
-    # TODO: start a connection
-    # server = CoAP.SocketServer.start_link([0, {CoAP.Adapters.Client, self()}])
     {:ok, connection} = CoAP.Connection.start_link([self(), {host, port, token}])
 
-    # TODO: deliver request
     send(connection, {:deliver, message})
 
-    # TODO: await response
     await_response(message)
   end
 
   defp await_response(_message) do
     receive do
       {:deliver, response, _peer} -> response
+      # TODO: do we need to re-request when we get an ack?
+      :ack -> :ack
     after
       @wait_timeout -> %Message{}
     end
