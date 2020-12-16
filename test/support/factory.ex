a15f06afad8967dc11c0065c24abde94c97e4627
Jean Parpaillon <jean.parpaillon@free.fr>
2020-12-16 17:39:38 +0100

    Apply mix format

--- test/support/factory.ex
+++ test/support/factory.ex
@@ -52,18 +52,18 @@ defmodule CoAP.Test.Support.Factory do
   @doc false
   def multipart do
     gen all(
-      description <- one_of([block(), constant(nil)]),
-      control <- one_of([block(), constant(nil)])
-      ) do
+          description <- one_of([block(), constant(nil)]),
+          control <- one_of([block(), constant(nil)])
+        ) do
       %Multipart{
         multipart: true,
         description: description,
         control: control,
-        more: (if description, do: description.more, else: nil),
-        number: (if description, do: description.number, else: nil),
-        size: (if description, do: description.size, else: nil),
-        requested_number: (if control, do: control.number, else: nil),
-        requested_size: (if control, do: control.size, else: nil)
+        more: if(description, do: description.more, else: nil),
+        number: if(description, do: description.number, else: nil),
+        size: if(description, do: description.size, else: nil),
+        requested_number: if(control, do: control.number, else: nil),
+        requested_size: if(control, do: control.size, else: nil)
       }
     end
   end
@@ -71,10 +71,10 @@ defmodule CoAP.Test.Support.Factory do
   @doc false
   def block do
     gen all(
-      number <- integer(0..65_535),
-      more <- boolean(),
-      size <- integer(0..65_535)
-    ) do
+          number <- integer(0..65_535),
+          more <- boolean(),
+          size <- integer(0..65_535)
+        ) do
       %Block{number: number, more: more, size: size}
     end
   end
@@ -82,7 +82,8 @@ defmodule CoAP.Test.Support.Factory do
   @doc false
   def options do
     gen all(
-          options <- map_of(integer(0..65_535), string(:ascii, max_length: 12 * 8), max_length: 16)
+          options <-
+            map_of(integer(0..65_535), string(:ascii, max_length: 12 * 8), max_length: 16)
         ) do
       options
     end
