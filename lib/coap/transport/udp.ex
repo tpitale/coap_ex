2ebcc324548d5c477067f20baa6a39792fd56137
Jean Parpaillon <jean.parpaillon@free.fr>
2020-12-16 17:38:33 +0100

    Fix credo warnings

--- lib/coap/transport/udp.ex
+++ lib/coap/transport/udp.ex
@@ -44,10 +44,11 @@ defmodule CoAP.Transport.UDP do
   @impl GenServer
   def init({host_ip, port, transport}) do
     # Open UDP socket for sending request, as 'client'
-    with {:ok, socket} <- :gen_udp.open(0, [:binary, {:active, true}, {:reuseaddr, true}]) do
-      {:ok, %__MODULE__{socket: socket, host: host_ip, port: port, transport: transport},
-       @timeout}
-    else
+    case :gen_udp.open(0, [:binary, {:active, true}, {:reuseaddr, true}]) do
+      {:ok, socket} ->
+        {:ok, %__MODULE__{socket: socket, host: host_ip, port: port, transport: transport},
+         @timeout}
+
       {:error, reason} ->
         {:error, reason}
     end
--- test/support/factory.ex
+++ test/support/factory.ex
@@ -26,11 +26,11 @@ defmodule CoAP.Test.Support.Factory do
               constant(nil),
               tuple({atom(:alphanumeric), atom(:alphanumeric)})
             ]),
-          message_id <- integer(0..65535),
+          message_id <- integer(0..65_535),
           token <- binary(min_length: 0, max_length: 64),
           options <- options(),
           multipart <- one_of([multipart(), constant(nil)]),
-          payload <- binary(min_length: 0, max_length: 65535)
+          payload <- binary(min_length: 0, max_length: 65_535)
         ) do
       %Message{
         version: 1,
@@ -71,9 +71,9 @@ defmodule CoAP.Test.Support.Factory do
   @doc false
   def block do
     gen all(
-      number <- integer(0..65535),
+      number <- integer(0..65_535),
       more <- boolean(),
-      size <- integer(0..65535)
+      size <- integer(0..65_535)
     ) do
       %Block{number: number, more: more, size: size}
     end
@@ -82,7 +82,7 @@ defmodule CoAP.Test.Support.Factory do
   @doc false
   def options do
     gen all(
-          options <- map_of(integer(0..65535), string(:ascii, max_length: 12 * 8), max_length: 16)
+          options <- map_of(integer(0..65_535), string(:ascii, max_length: 12 * 8), max_length: 16)
         ) do
       options
     end
